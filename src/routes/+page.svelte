75a32e976de36fb4c69f39defc6ddd66f118740d
Whizma <isak@maatta.se>
2023-02-21 20:14:58 +0100

    svelte verkar riktigt najz, la till ostylad navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,18 @@
+<script>
+
+  export let name = "world";
+  import Navbar from "./Navbar.svelte";
+
+</script>
+
+<Navbar />
+
 <div class="flex flex-col items-center justify-center h-screen">
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
+  <h1 class="text-3xl font-bold">
+    Hello {name}!
   </h1>
 </div>
+
 
 <style lang="postcss">
   :global(html) {
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,18 @@
+<Navbar />
+
+<div class="flex flex-col items-center justify-center h-screen">
+  <h1 class="text-3xl font-bold">
+    testar ny {name}!
+  </h1>
+</div>
+
+
+<script>
+  export let name = "komponent";
+  console.log("testar ny komponent");
+  import Navbar from "../Navbar.svelte";
+  import { onMount } from "svelte";
+  onMount(() => {
+    console.log("testar navbar");
+  });
+</script>
