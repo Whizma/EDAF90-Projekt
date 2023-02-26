91ed23afce37abc61bd38d6d97335148ad7bc44a
Whizma <isak@maatta.se>
2023-02-26 16:48:38 +0100

    städat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
 	import { onMount, onDestroy } from 'svelte';
-	import Navbar from './Navbar.svelte';
 	const carouselPhotos = [
 		'https://foodtank.com/wp-content/uploads/2021/07/alfons-morales-YLSwjSy7stw-unsplash.jpg',
 		'https://img-cdn.inc.com/image/upload/w_1920,h_1080,c_fill/images/panoramic/GettyImages-577674005_492115_zfpgiw.jpg',
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	export let name = 'komponent';
-	import Navbar from '../Navbar.svelte';
 </script>
 
 <div class="flex flex-col items-center justify-center h-screen">
