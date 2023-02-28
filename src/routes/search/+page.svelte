4e5f43d40f7171b5e9c86c656a09b62d920fcb59
Whizma <isak@maatta.se>
2023-02-28 15:54:58 +0100

    fett fult ska fixas

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
 	import { _searchOpenLibrary } from './+page';
 	import type { SearchResult } from './+page';
-	import { _searchForCover } from './+page';
 	import { Card, Search, Heading, List, Li, Span, Button } from 'flowbite-svelte';
 	import { writable } from 'svelte/store';
 
 	const searchResults = writable<SearchResult[]>([]);
 	let query = '';
+	let searchMade = false;
 
 	async function onSubmit() {
 		try {
 			const results = await _searchOpenLibrary(query);
 			searchResults.set(results);
+			searchMade = true;
 		} catch (error) {
 			console.error('An error occurred:', error);
 			searchResults.set([]);
+			searchMade = true;
 		}
 	}
 
@@ -31,35 +33,38 @@
 				stroke="currentColor"
 				viewBox="0 0 24 24"
 				xmlns="http://www.w3.org/2000/svg"
-				><path
+			>
+				<path
 					stroke-linecap="round"
 					stroke-linejoin="round"
 					stroke-width="2"
 					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-				/></svg
-			>
+				/>
+			</svg>
 		</Button>
 	</form>
 </div>
 
-<div class="pt-2 flex flex-col items-center justify-center">
-	<h1 class="flex text-2xl font-semibold">Search results</h1>
-	{#if results.length}
-		{#each results as result}
-			<div class="block mx-2 items-center w-1/6">
-				<Card href="https://openlibrary.org/" class="items-center">
-					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-						{result.title}
-					</h5>
-					<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
-						first published in {result.first_publish_year} by {#if result.author_name}{result.author_name.join(
-								', '
-							)}{:else}Unknown{/if}
-					</p>
-				</Card>
-			</div>
-		{/each}
-	{:else}
-		<p>No results found.</p>
-	{/if}
-</div>
+{#if searchMade}
+	<div class="pt-2 flex flex-col items-center justify-center">
+		<h1 class="flex text-2xl font-semibold">Search results</h1>
+		{#if results.length}
+			{#each results as result}
+				<div class="block mx-2 items-center w-1/6">
+					<Card href="https://openlibrary.org/" class="items-center">
+						<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+							{result.title}
+						</h5>
+						<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+							by {#if result.author_name}{result.author_name.join(', ')}
+							<img alt ="" src="https://covers.openlibrary.org/b/isbn/{result.isbn[0]}-L.jpg"/>
+							{:else}Unknown{/if}
+						</p>
+					</Card>
+				</div>
+			{/each}
+		{:else}
+			<p>No results found.</p>
+		{/if}
+	</div>
+{/if}
