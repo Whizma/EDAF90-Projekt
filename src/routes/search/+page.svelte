5149916f9454d6464a1041ebc968c336a9cb8aea
Whizma <isak@maatta.se>
2023-02-28 12:03:38 +0100

    sök funkar men fult

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { _searchOpenLibrary } from './+page';
 	import type { SearchResult } from './+page';
+  import { Search, Heading, List, Li, Span, Button } from 'flowbite-svelte';
 	import { writable } from 'svelte/store';
 
 	const searchResults = writable<SearchResult[]>([]);
@@ -19,11 +20,21 @@
 	$: results = $searchResults;
 </script>
 
+<form class="flex gap-2" on:submit|preventDefault={onSubmit}>
+  <Search type="text" bind:value={query} size="md" />
+  <Button type="submit" class="!p-2.5">
+    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/></svg>
+  </Button>
+</form>
+
 <div>
-	<form on:submit|preventDefault={onSubmit}>
+	<!-- <form on:submit|preventDefault={onSubmit}>
 		<input type="text" bind:value={query} />
-		<button type="submit">Search</button>
-	</form>
+    <Search >
+      <button> Search </button>
+    </Search>
+		 <button type="submit">Search</button> 
+	</form> -->
 
 	{#if results.length}
 		<ul>
