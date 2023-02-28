78be32a270523a805476b439e4ad79b2c2c3497d
Whizma <isak@maatta.se>
2023-02-28 15:37:35 +0100

    kommenterat kod!

--- src/routes/search/+page.ts
+++ src/routes/search/+page.ts
@@ -11,6 +11,9 @@ export const _searchOpenLibrary = async (query: string): Promise<SearchResult[]>
 	return json.docs;
 };
 
+// note: vill försöka hämta bild från isbn efter sökningen är gjord 
+// 			 så man kan se bokens framsida jämte informationen
+//       typ såhär https://flowbite-svelte.com/components/card#Horizontal_card
 export const _searchForCover = async (isbn: string): Promise<string> => {
 	const response = await fetch(`https://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`);
 	return response.url;
