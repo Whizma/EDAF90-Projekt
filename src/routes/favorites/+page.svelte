2e305cb3011df02199e62dd88b4c03a741a21c95
Whizma <isak@maatta.se>
2023-02-28 15:29:19 +0100

    fixar och trixar lite!

--- src/routes/favorites/+page.svelte
+++ src/routes/favorites/+page.svelte
@@ -3,8 +3,5 @@
 </script>
 
 <div class="flex flex-col items-center justify-center h-screen">
-	<h1 class="text-3xl font-bold">
-
-		Mina favoriter!
-	</h1>
+	<h1 class="text-3xl font-bold">Mina favoriter!</h1>
 </div>
