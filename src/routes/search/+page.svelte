d68699a9c6ff28fb2d6001b0662a036567700551
ErikM <er8540ma-s@student.lu.se>
2023-03-02 10:35:23 +0100

    Snyggade till advanced search knappen

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -28,7 +28,7 @@
 		isTextShown = !isTextShown;
 	};
 
-	$: buttonText = isTextShown ? 'Hide advanced' : 'Show advanced';
+	$: buttonText = isTextShown ? 'Hide filters' : 'Show filters';
 
 	let formData = new FormData();
 
@@ -160,21 +160,8 @@
 			</div>
 		</div>
 		<div style="display: flex; justify-content: center; align-items: center; margin-top: 10px;">
-		<Button type="submit" class="!p-2.5">
-			<svg
-				class="w-5 h-5"
-				fill="none"
-				stroke="currentColor"
-				viewBox="0 0 24 24"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-				/>
-			</svg>
+		<Button type="submit" class="!p-2.5" style="width: 200px;">
+			 <span class="">Filter</span>
 		</Button>
 	</div>
 	</form>
