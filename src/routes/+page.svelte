f48eebb1be18fb0b7bf3c64913f0a7b786b20420
Whizma <isak@maatta.se>
2023-03-06 10:25:58 +0100

    kkkkkk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition';
-	import { onMount, onDestroy } from 'svelte';
+  import { onMount, onDestroy } from 'svelte';
+  import { fade } from 'svelte/transition';
 	const carouselPhotos = [
-		'https://i.ebayimg.com/images/g/JwsAAOSwfXlgRtzJ/s-l1600.jpg',
-		//'https://covers.openlibrary.org/b/isbn/9780717802418-L.jpg'
+		'https://covers.openlibrary.org/b/id/13217026-L.jpg',
+		'https://covers.openlibrary.org/b/isbn/9780717802418-L.jpg',
+    'https://covers.openlibrary.org/b/id/13257896-L.jpg',
+    'https://covers.openlibrary.org/b/id/12882777-L.jpg',
+    'https://covers.openlibrary.org/b/id/12670684-L.jpg',
+	'https://covers.openlibrary.org/b/id/13244471-L.jpg',
+	'https://covers.openlibrary.org/b/id/12993656-L.jpg',
+	'https://covers.openlibrary.org/b/id/13008986-L.jpg',
+	'https://covers.openlibrary.org/b/id/12145320-L.jpg',
+  'https://covers.openlibrary.org/b/id/11550609-L.jpg',
+  'https://covers.openlibrary.org/b/id/13217026-L.jpg',
 	];
 
 	let index = 0;
@@ -12,14 +21,15 @@
 	const next = () => {
 		index = (index + 1) % carouselPhotos.length;
 	};
-/*
+
 	onMount(() => {
 		interval = window.setInterval(next, 5000);
 	});
 
 	onDestroy(() => {
 		clearInterval(interval);
-	});*/
+	});
+  
 </script>
 
 {#each [carouselPhotos[index]] as src (index)}
@@ -30,7 +40,7 @@
 	img {
 		display: block;
 		margin: auto;
-		width: 100%;
+		width: 30%;
 		height: auto;
 	}
 </style>
